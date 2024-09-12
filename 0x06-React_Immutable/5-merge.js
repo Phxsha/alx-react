cadbead0f05fc661b522a04e9c94eb0efbcb4e9c
root <phashateboho@gmail.com>
2024-09-12 19:12:37 +0000

    task 6

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,14 +1,5 @@
 import { List, Map } from 'immutable';
 
-// Function to concatenate two arrays into an Immutable List
-export function concatElements(page1, page2) {
-  return List(page1.concat(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-// Function to merge two objects into an Immutable List
-// Values from page2 will override values from page1 if they are the same
-export function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  return List(map2.merge(map1).values());
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
